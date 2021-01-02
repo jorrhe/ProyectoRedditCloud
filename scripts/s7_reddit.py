c4f5ee12ef12471829a815bdf52c66ca7f2251e9
FJavierLoz <60520365+FJavierLoz@users.noreply.github.com>
2021-01-02 22:05:48 +0100

    Update s7_reddit.py

--- scripts/s7_reddit.py
+++ scripts/s7_reddit.py
@@ -1,12 +1,11 @@
 # Cloud y Big Data
 # Realizado por Jorge Rosello, Daniel Alcazar, Francisco Javier Lozano
 # Nombre Script: S7
-# Descripcion: Extrae el numero de posts nsfw de cada subreddit,
-# contrastandolo frente a su numero total de posts e indicando el porcentaje de estos que son nsfw
+# Descripcion: Extrae el numero de posts nsfw de cada subreddit.
 
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as f
+from pyspark.sql.functions import col
 from pushshift import file_to_dataframe
 
 conf = SparkConf().setAppName('S7')
@@ -14,13 +13,6 @@ sc = SparkContext(conf=conf)
 ss = SparkSession(sc)
 df = file_to_dataframe('RS_2019-01', ss)
 
-df_total_post_count = df.groupBy(f.col("subreddit")).count()\
-    .select("subreddit", f.col("count").alias("total_posts"))
-
-df_nsfw_post_count = df.filter(f.col("over_18") == True)\
-    .groupBy(f.col("subreddit")).count().\
-    select("subreddit", f.col("count").alias("nsfw_posts"))
-
-df_nsfw_post_count.join(df_total_post_count, on=["subreddit"], how="inner").\
-    withColumn("percentage", (f.col("nsfw_posts")/f.col("total_posts"))*100)\
-    .write.json("s7_salida")
+df.filter(col("over_18") == True)\
+    .groupBy(col("subreddit")).count().\
+    select("subreddit", col("count").alias("nsfw_posts")).write.json("s7_salida")
