c7e03db870290e95b1012ae671abb14f9130ee89
FJavierLoz <60520365+FJavierLoz@users.noreply.github.com>
2021-01-02 19:36:02 +0100

    Update s5_reddit.py

--- scripts/s5_reddit.py
+++ scripts/s5_reddit.py
@@ -1,17 +1,17 @@
 # Cloud y Big Data
 # Realizado por Jorge Rosello, Daniel Alcazar, Francisco Javier Lozano
 # Nombre Script: S5
-# Descripcion: Extrae la lista de las 10 palabras más usadas en el cuerpo en los 10 posts con más votos
+# Descripcion: Extrae la lista de las 10 palabras mas usadas en el cuerpo en los 10 posts con mas votos
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
 from pushshift import file_to_dataframe
 arr= ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
-conf = SparkConf().setAppName('s5')
+conf = SparkConf().setAppName('S5')
 sc = SparkContext(conf=conf)
 ss = SparkSession(sc)
-df = file_to_dataframe('ficheros/Ejemplo10000Filas.json', ss)
+df = file_to_dataframe('RS_2019-01', ss)
 top_10_posts = df.filter((f.col("title") != "") & (f.col("title") != "[deleted]") & (f.col("title") != "[removed]"))\
     .orderBy("score", ascending=False)\
     .limit(10)
