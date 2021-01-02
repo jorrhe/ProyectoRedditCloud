ed06977967fd919183e55a43ed7b13d700207774
Daniel <daalca01@ucm.es>
2021-01-02 19:34:23 +0100

    Retocados S8,S9, S10 Y S11 para que funcione en cluster

--- scripts/s8_reddit.py
+++ scripts/s8_reddit.py
@@ -7,7 +7,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pushshift import file_to_dataframe
 
-conf = SparkConf().setAppName('subredditsTop10Score')
+conf = SparkConf().setAppName('S8')
 sc = SparkContext(conf=conf)
 ss = SparkSession(sc)
 
