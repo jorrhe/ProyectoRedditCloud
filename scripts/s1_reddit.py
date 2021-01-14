ff1e579a1ea8a266a7111330ee50941236b28d3e
Jorge <jorgebeybo@hotmail.com>
2021-01-14 14:20:16 +0100

    Modificado S1 para que acepte un fichero de forma paramétrica

--- scripts/s1_reddit.py
+++ scripts/s1_reddit.py
@@ -5,15 +5,33 @@
 
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_unixtime, to_timestamp, hour, avg
+from pyspark.sql.functions import from_unixtime, avg
 
 from pushshift import file_to_dataframe
+
+import argparse
+import sys
+from os import path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="Path where the dataset is located")
+args = parser.parse_args()
+
+if not args.file:
+    print("Error --file argument not passed")
+    sys.exit()
+
+file_path = args.file
+
+if not path.exists(file_path):
+    print("Error %s does not exist" % file_path)
+    sys.exit()
 
 conf = SparkConf().setAppName('S1')
 sc = SparkContext(conf=conf)
 ss = SparkSession(sc)
 
-df = file_to_dataframe('RS_2019-01', ss)
+df = file_to_dataframe(file_path, ss)
 
 df.select(
     'score',
